c7733cf23e3ec96e03839f070bc8dcb18137ce93
Pavel Nemecek <pavel.nemecek@zentity.com>
2020-09-23 18:56:49 +0200

    FFMpeg

--- Encoding/Encoding/VodFunctions/ffmpeg-encoding.cs
+++ Encoding/Encoding/VodFunctions/ffmpeg-encoding.cs
@@ -7,8 +7,11 @@
 ```c#
 Input :
 {
-    "sasInputUrl":"",
-    "sasOutputUrl":"",
+    "inputFileName":"",
+    "storageAccount":"",
+    "storageKey":"",
+    "outputAccount":"",
+    "outputKey":"",
     "ffmpegArguments" : " -i {input} {output} -y"
 }
 
@@ -22,6 +25,7 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Net;
+using System.Text;
 using System.Threading.Tasks;
 using Encoding.Helpers;
 using Microsoft.AspNetCore.Http;
@@ -32,6 +36,8 @@ using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
 using Microsoft.WindowsAzure.Storage.Blob;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
@@ -40,6 +46,21 @@ namespace Encoding
 {
     public static class fmpeg
     {
+
+        public static async Task<List<IListBlobItem>> ListBlobsAsync(CloudBlobContainer container)
+        {
+            BlobContinuationToken continuationToken = null; //start at the beginning
+            var results = new List<IListBlobItem>();
+            do
+            {
+                var response = await container.ListBlobsSegmentedAsync(continuationToken);
+                continuationToken = response.ContinuationToken;
+                results.AddRange(response.Results);
+            }
+
+            while (continuationToken != null); //when this is null again, we've reached the end
+            return results;
+        }
 
         [FunctionName("ffmpeg-encoding")]
         public static async Task<IActionResult> Run(
@@ -64,29 +85,36 @@ namespace Encoding
                 return Helpers.Helpers.ReturnErrorException(log, ex);
             }
 
-            var ffmpegArguments = (string)data.ffmpegArguments;
+            var inputFileName = (string)data.inputFileName;
+            var storageAccount = (string)data.storageAccount;
+            var storageKey = (string)data.storageKey;
+            var outputAccount = (string)data.outputAccount;
+            var outputKey = (string)data.outputKey;
 
-            var sasInputUrl = (string)data.sasInputUrl;
-            if (sasInputUrl == null)
-                return Helpers.Helpers.ReturnErrorException(log, "Error - please pass sasInputUrl in the JSON");
+            StorageCredentials inpoutStorageCredentials = new StorageCredentials(storageAccount, storageKey);
+            CloudStorageAccount inputStorageAccount = new CloudStorageAccount(inpoutStorageCredentials, useHttps: true);
 
-            var sasOutputUrl = (string)data.sasOutputUrl;
-            if (sasOutputUrl == null)
-                return Helpers.Helpers.ReturnErrorException(log, "Error - please pass sasOutputUrl in the JSON");
+            StorageCredentials outputStorageCredentials = new StorageCredentials(outputAccount, outputKey);
+            CloudStorageAccount outputStorageAccount = new CloudStorageAccount(outputStorageCredentials, useHttps: true);
 
+            CloudBlobClient outputBlobClient = outputStorageAccount.CreateCloudBlobClient();
+            CloudBlobContainer outputContainer = outputBlobClient.GetContainerReference("pre-agora");
 
-            log.LogInformation("Arguments : ");
-            log.LogInformation(ffmpegArguments);
+            CloudBlobClient inputBlobClient = inputStorageAccount.CreateCloudBlobClient();
+            CloudBlobContainer inputContainer = inputBlobClient.GetContainerReference("upload-agora");
+
+            var blobs = await ListBlobsAsync(inputContainer);
+            var shortName = inputFileName.Replace(".m3u8", "");
+            var selectedBlobs = blobs.Cast<CloudBlockBlob>().Where(a=>a.Name.Contains(shortName)).Select(a=>a.Name).ToList();
 
             try
             {
                 var folder = context.FunctionDirectory;
                 var tempFolder = Path.GetTempPath();
 
-                string inputFileName = System.IO.Path.GetFileName(new Uri(sasInputUrl).LocalPath);
                 string pathLocalInput = System.IO.Path.Combine(tempFolder, inputFileName);
 
-                string outputFileName = System.IO.Path.GetFileName(new Uri(sasOutputUrl).LocalPath);
+                string outputFileName = Guid.NewGuid().ToString()+".mp4";
                 string pathLocalOutput = System.IO.Path.Combine(tempFolder, outputFileName);
 
                 foreach (DriveInfo drive in DriveInfo.GetDrives().Where(d => d.IsReady))
@@ -94,57 +122,69 @@ namespace Encoding
                     log.LogInformation($"{drive.Name}: {drive.TotalFreeSpace / 1024 / 1024} MB");
                 }
 
-                /* Downloads the original video file from blob to local storage. */
-                log.LogInformation("Dowloading source file from blob to local");
-                using (FileStream fs = System.IO.File.Create(pathLocalInput))
-                {
-                    try
+                foreach (var name in selectedBlobs) {
+                    string localPath = System.IO.Path.Combine(tempFolder, name);
+
+                    /* Downloads the original video file from blob to local storage. */
+                    log.LogInformation("Dowloading source files from blob to local");
+                    using (FileStream fs = System.IO.File.Create(localPath))
                     {
-                        var readBlob = new CloudBlob(new Uri(sasInputUrl));
-                        await readBlob.DownloadToStreamAsync(fs);
-                        log.LogInformation("Downloaded input file from blob");
-                    }
-                    catch (Exception ex)
-                    {
-                        log.LogError("There was a problem downloading input file from blob. " + ex.ToString());
+                        try
+                        {
+                            var readBlob = inputContainer.GetBlockBlobReference(name);
+                            await readBlob.DownloadToStreamAsync(fs);
+                            log.LogInformation("Downloaded input file from blob" + name);
+                        }
+                        catch (Exception ex)
+                        {
+                            log.LogError("There was a problem downloading input file from blob. " + ex.ToString());
+                        }
                     }
                 }
 
                 log.LogInformation("Encoding...");
                 var file = System.IO.Path.Combine(folder, "..\\ffmpeg\\ffmpeg.exe");
+                log.LogInformation($"ffmeg Exists: {File.Exists(file)}");
 
                 System.Diagnostics.Process process = new System.Diagnostics.Process();
+                process.StartInfo.WorkingDirectory = tempFolder;
                 process.StartInfo.FileName = file;
 
-                process.StartInfo.Arguments = (ffmpegArguments ?? " -i {input} {output} -y")
+                log.LogInformation($"Temp In Exists: {File.Exists(pathLocalInput)}");
+
+
+                process.StartInfo.Arguments = ("-i {input} -acodec copy -bsf:a aac_adtstoasc -vcodec copy {output}")
                     .Replace("{input}", "\"" + pathLocalInput + "\"")
                     .Replace("{output}", "\"" + pathLocalOutput + "\"")
                     .Replace("'", "\"");
 
                 log.LogInformation(process.StartInfo.Arguments);
-
+                process.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                process.StartInfo.CreateNoWindow = true;
                 process.StartInfo.UseShellExecute = false;
                 process.StartInfo.RedirectStandardOutput = true;
                 process.StartInfo.RedirectStandardError = true;
-
+                var sb = new StringBuilder();
                 process.OutputDataReceived += new DataReceivedEventHandler(
                     (s, e) =>
                     {
                         log.LogInformation("O: " + e.Data);
                     }
                 );
-                process.ErrorDataReceived += new DataReceivedEventHandler(
-                    (s, e) =>
-                    {
-                        log.LogInformation("E: " + e.Data);
-                    }
-                );
+               process.ErrorDataReceived += (s, a) => sb.AppendLine(a.Data);
+               process.EnableRaisingEvents = true;
+
                 //start process
                 process.Start();
                 log.LogInformation("process started");
                 process.BeginOutputReadLine();
                 process.BeginErrorReadLine();
                 process.WaitForExit();
+                if (process.ExitCode != 0)
+                {
+                    log.LogInformation(sb.ToString());
+                    throw new InvalidOperationException($"Ffmpeg failed with exit code {process.ExitCode}");
+                }
 
                 exitCode = process.ExitCode;
                 ffmpegResult = output;
@@ -153,11 +193,14 @@ namespace Encoding
 
                 /* Uploads the encoded video file from local to blob. */
                 log.LogInformation("Uploading encoded file to blob");
+
+                log.LogInformation($"Temp Out Exists: {File.Exists(pathLocalOutput)}");
+
                 using (FileStream fs = System.IO.File.OpenRead(pathLocalOutput))
                 {
                     try
                     {
-                        var writeBlob = new CloudBlockBlob(new Uri(sasOutputUrl));
+                        var writeBlob = outputContainer.GetBlockBlobReference(outputFileName);
                         await writeBlob.UploadFromStreamAsync(fs);
                         log.LogInformation("Uploaded encoded file to blob");
                     }
@@ -166,7 +209,16 @@ namespace Encoding
                         log.LogInformation("There was a problem uploading converted file to blob. " + ex.ToString());
                     }
                 }
-                System.IO.File.Delete(pathLocalInput);
+
+                var files = Directory.GetFiles(tempFolder, "*.*", SearchOption.AllDirectories);
+                foreach (var fll in files)
+                {
+                    if (File.Exists(fll))
+                    {
+                        File.Delete(fll);
+                    }
+                }
+
                 System.IO.File.Delete(pathLocalOutput);
             }
             catch (Exception e)
